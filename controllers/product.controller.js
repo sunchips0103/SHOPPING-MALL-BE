d52cd7a643e1a3818f32768a6c77ab0accde9e7e
sunchips0103 <rnrxodid@naver.com>
2025-05-13 23:00:59 +0900

    페이지 네이션 BE

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,8 @@
 const productController = {};
 const Product = require('../models/Product');
 // product 모델 참고해서 작성하면 됨
+
+const PAGE_SIZE=1;
 productController.createProduct = async (req, res) => {
   try {
     const {
@@ -39,9 +41,20 @@ productController.getProducts = async(req,res)=>{
         //regex란 정규표현식
         //options:"i" ->영어 대소문자 구분 x
       let query = Product.find(cond);
+      let response = {status:"success"};
+      if(page){
+        query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE) //limit 갖고싶은 데이터 최대수 ex 10개중 5개를 가지고 싶다.  skip 데이터를 건너뜀
+        //최종 몇개 페이지
+        //데이터가 총 몇개있는지 확인
+        const totalItemNum = await Product.countDocuments(cond);   
+        //테이터 총갯수 / PAGE_SIZE
+        const totalPageNum = Math.ceil(totalItemNum/PAGE_SIZE);
+        response.totalPageNum=totalPageNum;
+      }
 
       const productList = await query.exec();
-      res.status(200).json({status:"success",data:productList})
+      response.data=productList
+      res.status(200).json(response);
     }catch(error){
       res.status(400).json({ status: 'fail', error: error.message });
     }
