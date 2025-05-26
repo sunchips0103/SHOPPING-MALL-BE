fe10ba4c023af1860d66ef7c849f7c9a8407e73b
sunchips0103 <rnrxodid@naver.com>
2025-05-26 23:34:11 +0900

    구글 로그인

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken")
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const {OAuth2Client} = require("google-auth-library");
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID;
 
 const authController = {}
 
@@ -21,7 +23,43 @@ authController.loginWithEmail = async(req,res)=>{
     }catch(error){
         res.status(400).json({status:"fail", error:error.message})
     }
-}
+};
+authController.loginWithGoogle =async(req,res)=>{
+   try {
+    const {token} = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    const {email, name} = ticket.getPayload();
+    console.log("eee", email, name);
+    let user = await User.findOne({email});
+    if (!user) {
+      //유저를 새로 생성
+      const randomPassword = ""+Math.floor(Math.random()*10000000);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({
+        name,
+        email,
+        password: newPassword,
+      });
+      await user.save()
+    }
+    //토큰 발행 후 리턴
+    const sessionToken = await user.generateToken();
+    res.status(200).json({status:"success",user,token:sessionToken})
+    }catch(error){
+        return res.status(400).json({status:"fail", error: error.message})
+
+    }
+};
+// 4. 백엔드에서 로그인하기
+        //토큰 값을 읽어와서 => 유저정보를 뽑아내고 email
+        //  a.이미 로그인 한적이 있는 유저->로그인시키고 토큰값 주면 장떙
+        //  b.처음 로그인을 시도를 한 유저다 -> 유저 정보 먼저 새로생성 => 토큰 값
+
 
 authController.authenticate = async(req,res,next)=>{
     try{
@@ -49,5 +87,6 @@ authController.checkAdminPermission = async(req,res,next) => {
         res.status(400).json({status:"fail",error:error.message})
     }
 }
+
 
 module.exports = authController
--- routes/auth.api.js
+++ routes/auth.api.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post('/login',authController.loginWithEmail); //요청시 body 보냄 get()은 body 안보냄
+router.post('/google',authController.loginWithGoogle); 
+
 
 module.exports =router;
